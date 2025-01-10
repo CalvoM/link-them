be41d8b135432a4bf7d8431fa310c4dcf2cd2b2e
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2025-01-10 19:50:56 +0300

    ui: We can capture the input from the user

---
+++ frontend/src/components/__tests__/TheSearchBar.spec.ts
@@ -0,0 +1,28 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import TheSearchBar from '../TheSearchBar.vue'
+describe("TheSearchBar", () => {
+  const actor_suggestions: Array<string> = new Array<string>("Alex Wamatu", "Abel Mutua", "DJ Shiti")
+  it('emits the submit event', () => {
+    const wrapper = mount(TheSearchBar, { props: { actors: actor_suggestions } })
+    const actorOneAutocomplete = wrapper.find("input#srcActor")
+    const actorTwoAutocomplete = wrapper.find("input#destActor")
+    actorOneAutocomplete.setValue(actor_suggestions[0])
+    actorTwoAutocomplete.setValue(actor_suggestions[1])
+    const submitButton = wrapper.find("button")
+    submitButton.trigger("click")
+    const submitEvent = wrapper.emitted()
+    expect(submitEvent).toHaveProperty("submit")
+  })
+  it("Displays messge when submitted without values", () => {
+    const wrapper = mount(TheSearchBar, { props: { actors: actor_suggestions } })
+    const submitButton = wrapper.find("button")
+    // const msg1 = wrapper.get("div#msg1")
+    // const msg2 = wrapper.get("div#msg2")
+    expect(wrapper.find("#msg1").isVisible()).toBe(false)
+    expect(wrapper.find("#msg2").isVisible()).toBe(false)
+    submitButton.trigger("click")
+    expect(wrapper.find("#msg1").isVisible()).toBe(true)
+    expect(wrapper.find("#msg2").isVisible()).toBe(true)
+  })
+})
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+import TheSearchBar from '@/components/TheSearchBar.vue';
+import { ref, type Ref } from 'vue';
+const actors: Ref<Array<string>> = ref([])
+actors.value = new Array<string>("One", "Two", "Three")
+function submitRequest(srcActor: string, destActor: string) {
+  console.log(srcActor)
+  console.log(destActor)
 
+}
+</script>
 <template>
   <main>
-    <TheWelcome />
+    <TheSearchBar :actors="actors" @submit="submitRequest" />
   </main>
 </template>
