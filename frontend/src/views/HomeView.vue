7e10094bfbfc9a417ecc9786a9f99ad5d1f53229
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2025-01-11 12:29:37 +0300

    ui: added prime framework

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import TheSearchBar from '@/components/TheSearchBar.vue';
-import { ref, type Ref } from 'vue';
+import TheSearchBar from '@/components/TheSearchBar.vue'
+import { ref, type Ref } from 'vue'
 const actors: Ref<Array<string>> = ref([])
-actors.value = new Array<string>("One", "Two", "Three")
+actors.value = new Array<string>('One', 'Two', 'Three')
 function submitRequest(srcActor: string, destActor: string) {
   console.log(srcActor)
   console.log(destActor)
-
 }
 </script>
 <template>
