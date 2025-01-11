3039fe8b767fab59a712b112c4fd770a4cdb38a9
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2025-01-11 12:29:37 +0300

    ui: added prime framework

--- frontend/src/components/TheSearchBar.vue
+++ frontend/src/components/TheSearchBar.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import { AutoComplete, type AutoCompleteCompleteEvent } from 'primevue';
+import { AutoComplete, type AutoCompleteCompleteEvent } from 'primevue'
 import { ref, type Ref } from 'vue'
 const srcActor: Ref<string> = ref('')
 const destActor: Ref<string> = ref('')
 const props = defineProps<{
   actors: Array<string>
-}>();
+}>()
 const emit = defineEmits<{
   submit: [srcActor: string, destActor: string]
-}>();
+}>()
 const suggestedActors: Ref<Array<string>> = ref([])
 const clickedSubmit: Ref<boolean> = ref(false)
 
@@ -17,26 +17,49 @@ function search() {
   emit('submit', srcActor.value, destActor.value)
 }
 function getActors(event: AutoCompleteCompleteEvent) {
-  suggestedActors.value = props.actors.filter((actor: string) => actor.toLowerCase().includes(event.query.toLowerCase()))
+  suggestedActors.value = props.actors.filter((actor: string) =>
+    actor.toLowerCase().includes(event.query.toLowerCase()),
+  )
 }
 </script>
 <template>
   <div class="flex justify-evenly">
     <FloatLabel variant="on" class="flex-none">
-      <AutoComplete class="mb-2" inputId="srcActor" id="srcActor" type="text" v-model="srcActor"
-        :suggestions="suggestedActors" @complete="getActors" />
+      <AutoComplete
+        class="mb-2"
+        inputId="srcActor"
+        id="srcActor"
+        type="text"
+        v-model="srcActor"
+        :suggestions="suggestedActors"
+        @complete="getActors"
+      />
       <label for="srcActor">Starting Actor</label>
     </FloatLabel>
-    <Message icon="pi pi-times-circle" severity="error" v-if="!srcActor && clickedSubmit" id="msg1">Actor is
-      needed</Message>
+    <Message icon="pi pi-times-circle" severity="error" v-if="!srcActor && clickedSubmit" id="msg1"
+      >Actor is needed</Message
+    >
     <FloatLabel variant="on" class="flex-none">
-      <AutoComplete inputId="destActor" type="text" id="destActor" v-model="destActor" :suggestions="suggestedActors"
-        @complete="getActors" />
+      <AutoComplete
+        inputId="destActor"
+        type="text"
+        id="destActor"
+        v-model="destActor"
+        :suggestions="suggestedActors"
+        @complete="getActors"
+      />
       <label for="destActor">Final Actor</label>
     </FloatLabel>
-    <Message icon="pi pi-times-circle" severity="error" v-if="!destActor && clickedSubmit" id="msg2">Actor is
-      needed</Message>
-    <Button label="Find link" aria-label="Search" icon="pi pi-search" severity="success" class="flex-none"
-      @click="search" />
+    <Message icon="pi pi-times-circle" severity="error" v-if="!destActor && clickedSubmit" id="msg2"
+      >Actor is needed</Message
+    >
+    <Button
+      label="Find link"
+      aria-label="Search"
+      icon="pi pi-search"
+      severity="success"
+      class="flex-none"
+      @click="search"
+    />
   </div>
 </template>
