232be2e54bfd95db4da9ed405b202d62a02786c5
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2025-01-16 22:57:22 +0300

    feat(actors): We can consume the actors details

---
+++ backend/README.md
@@ -0,0 +1,24 @@
+# link-them
+
+I am learning about graphs, so I making this app to learn how to do graphs in apps.
+
+## Plan
+
+### Roadmap of tasks
+
+- [ ] Get movies
+- [ ] Get actors in movies
+- [ ] Get connection between actors in same movie
+- [ ] Get connection between actors in different movies via other actors
+- [ ] Continuously update the records of movies and actors
+
+### Development
+
+- Frontend -> **Vue**
+- Backend  -> **Golang**
+- Database -> **Postgres** (*Through Docker*)
+- Background service -> **TBD**
+
+## Credits
+
+The data used is from [TMDB](https://www.themoviedb.org/)
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,10 +5,26 @@ import type { ActorAutocompleteDetails } from '@/types'
 import { ref, type Ref } from 'vue'
 const auth = useAuthStore()
 const actors: Ref<Array<ActorAutocompleteDetails>> = ref([])
-// actors.value = new Array<string>('One', 'Two', 'Three')
-function submitRequest(srcActor: string, destActor: string) {
-  console.log(srcActor)
-  console.log(destActor)
+
+async function submitRequest(srcActor: string, destActor: string) {
+  const srcActorDetails = actors.value
+    .filter((actor: ActorAutocompleteDetails) => actor.name === srcActor)
+    .map((actor: ActorAutocompleteDetails) => ({
+      srcActor: actor.name,
+      srcActorID: actor.id,
+    }))[0]
+  const destActorDetails = actors.value
+    .filter((actor: ActorAutocompleteDetails) => actor.name === destActor)
+    .map((actor: ActorAutocompleteDetails) => ({
+      destActor: actor.name,
+      destActorID: actor.id,
+    }))[0]
+  console.log(srcActorDetails)
+  console.log(destActorDetails)
+  await auth.postResource('/server/actors', {
+    ...srcActorDetails,
+    ...destActorDetails,
+  })
 }
 async function getActors() {
   await auth.getResource('/server/actors')
