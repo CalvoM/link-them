92ddba17fad2c9fe924159ef6bd85f695e2d7e7e
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2025-01-21 16:22:15 +0300

    feat(same movie connection): We can get the movie ids
    We are yet to now properly render the connection using the
    relation-graph

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import TheResultsDisplay from '@/components/TheResultsDisplay.vue'
 import TheSearchBar from '@/components/TheSearchBar.vue'
 import { useAuthStore } from '@/stores/auth'
 import type { ActorAutocompleteDetails } from '@/types'
@@ -6,34 +7,22 @@ import { ref, type Ref } from 'vue'
 const auth = useAuthStore()
 const actors: Ref<Array<ActorAutocompleteDetails>> = ref([])
 
-async function submitRequest(srcActor: string, destActor: string) {
-  const srcActorDetails = actors.value
-    .filter((actor: ActorAutocompleteDetails) => actor.name === srcActor)
-    .map((actor: ActorAutocompleteDetails) => ({
-      srcActor: actor.name,
-      srcActorID: actor.id,
-    }))[0]
-  const destActorDetails = actors.value
-    .filter((actor: ActorAutocompleteDetails) => actor.name === destActor)
-    .map((actor: ActorAutocompleteDetails) => ({
-      destActor: actor.name,
-      destActorID: actor.id,
-    }))[0]
-  console.log(srcActorDetails)
-  console.log(destActorDetails)
+async function submitRequest(srcActor: object, destActor: object) {
+  console.log({ ...srcActor, ...destActor })
   await auth.postResource('/server/actors', {
-    ...srcActorDetails,
-    ...destActorDetails,
+    ...srcActor,
+    ...destActor,
   })
 }
 async function getActors() {
   await auth.getResource('/server/actors')
   actors.value = auth.resourceData as Array<ActorAutocompleteDetails>
 }
-getActors()
+// getActors()
 </script>
 <template>
   <main>
     <TheSearchBar :actors="actors" @submit="submitRequest" />
+    <TheResultsDisplay />
   </main>
 </template>
